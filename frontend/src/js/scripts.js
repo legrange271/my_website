055ac4bd3c6a1afca99146afd2e59bf2cc9dd934
Henry Ling <henrytling@hotmail.co.uk>
2022-11-17 18:09:25 +0000

    added css and js files and added a more interesting frontend.

---
+++ frontend/src/js/scripts.js
@@ -0,0 +1,24 @@
+function SendAlert() {
+	alert("Hello - thnak you to coming to my website");
+}
+
+function ShowHidden(){
+	document.getElementById("hiddentext").style.display = 'block';
+}
+
+function updateTheNumber(){
+	var currentVal = parseFloat(document.getElementById("numbertoupdate").innerHTML);
+	console.log(currentVal)
+	console.log(typeof currentVal)
+	if (currentVal == 3) {
+		var newVal = Math.PI;
+	}
+	else if (currentVal > 10){
+		var newVal = Math.floor(Math.random()*100)
+	}
+	else {
+		var newVal = currentVal +1;
+	}
+	document.getElementById("numbertoupdate").innerHTML = newVal
+
+}
